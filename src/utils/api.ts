8ce118b5577531452ab0bc4e9ae18d16e9f9173e
puneeth <puneethck24@gmail.com>
2026-07-20 19:02:27 +0530

    only for deployment

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://nexus-app-server.onrender.com",
   withCredentials: true,
 });
 
