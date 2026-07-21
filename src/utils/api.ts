c10ae46e218fc696974552c2473113fc59a4e852
puneeth <puneethck24@gmail.com>
2026-07-21 12:55:01 +0530

    backend url env change

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://nexus-app-server.onrender.com/api",
+  baseURL: import.meta.env.BACKEND_URL,
   withCredentials: true,
 });
 
