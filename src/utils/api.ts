f2a3f31f2f2480951cd892284951b62bfca70883
puneeth <puneethck24@gmail.com>
2026-07-21 10:36:07 +0530

    "api url correction in frontend"

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://nexus-app-server.onrender.com",
+  baseURL: "https://nexus-app-server.onrender.com/api",
   withCredentials: true,
 });
 
